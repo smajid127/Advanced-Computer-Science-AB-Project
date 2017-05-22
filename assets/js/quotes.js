64c351fac5665f7c2c252ad89045660fb42262a5
ericzgy <ericzgy2008@gmail.com>
2017-05-22 12:48:14 -0400

    Add files via upload

--- assets/js/quotes.js
+++ assets/js/quotes.js
@@ -1,9 +1,16 @@
-var quotes = {
-	'a',
-	'b',
-	'c',
-	'd',
-	'e',
-	'f',
-}
-*$(document).ready(function()
+		$(document).ready(function(){
+			var quotes = ["Eric Guan", "Gandhi", "Mr. Lau", "Daniel Shin", "Edmund Lau"];
+			var randomNumber = Math.floor(Math.random()*(quotes.length));
+			var actualQuote = quotes[randomNumber];
+			var myMap = new Map();
+			myMap.set("Eric Guan", "hi");
+			myMap.set("Gandhi", "hello");
+			myMap.set("Mr. Lau", "oh");
+			myMap.set("Daniel Shin", "yes");
+			myMap.set("Edmund Lau", "yeet");
+			$("#authorDisplay p").text(myMap.get(actualQuote));
+			$("#authorDisplay p").addClass("load");
+			$("#quoteDisplay p").text(actualQuote);
+			$("#quoteDisplay p").addClass("load");
+
+		});
